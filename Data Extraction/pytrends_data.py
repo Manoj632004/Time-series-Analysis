2f2643c9e9488813a70a1b7b00f15e055679fddc
Manoj632004 <manoj632004s@gmail.com>
2025-06-21 19:31:20 +0530

    Data extraction 002

---
+++ Data Extraction/pytrends_data.py
@@ -0,0 +1,23 @@
+from pytrends.request import TrendReq
+import pandas as pd
+
+pytrends = TrendReq(hl='en-US', tz=330)
+
+# Limit to 5 keywords max
+#keywords = ["Meta Quest", "Meta Quest 3", "Meta Quest Pro", "Oculus Quest", "Meta AR glasses"]
+keywords = [
+    "Meta AI",
+    "Meta AI app",
+    "Llama 4",
+    "LlamaCon",
+    "Meta Motivo",
+]
+
+
+pytrends.build_payload(kw_list=keywords, timeframe='2023-01-01 2025-01-01')
+
+data = pytrends.interest_over_time()
+data = data.drop(columns=['isPartial'])
+
+print(data.head())
+data.to_csv("meta_arvr_trends.csv")
